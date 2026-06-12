41f333e3644902979ebe37ca9471a9d22e9d6788
otusnoctua <raymantishq@gmail.com>
2026-06-13 01:57:46 +0300

    add entries packaging to zip

--- src/main/java/org/example/RegistryScraper.java
+++ src/main/java/org/example/RegistryScraper.java
@@ -3,10 +3,13 @@ package org.example;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class RegistryScraper {
 
@@ -22,47 +25,59 @@ public class RegistryScraper {
     public void scrapeAllData(String fromDate, String toDate) throws IOException {
         int pageNum = 1;
         int totalPages = Integer.MAX_VALUE;
+        int pageSize = 1000;
 
         String registryDirName = "epbs-registry-entries";
         Files.createDirectories(Paths.get(registryDirName));
+        String zipFilePath = String.format("%s/epbs_%s_%s.zip", registryDirName, fromDate, toDate);
 
-        while (pageNum <= totalPages) {
-            try {
-                int pageSize = 1000;
-                String jsonResponse = httpHandler.fetchRegistryEntries(fromDate, toDate, pageNum, pageSize);
-                JsonNode rootNode = objectMapper.readTree(jsonResponse);
+        try (FileOutputStream fos = new FileOutputStream(zipFilePath);
+             ZipOutputStream zos = new ZipOutputStream(fos)) {
 
-                if (rootNode == null || rootNode.isEmpty()) {
-                    System.out.println("Получен пустой ответ от сервера. Прерываю работу");
+            while (pageNum <= totalPages) {
+                try {
+                    String jsonResponse = httpHandler.fetchRegistryEntries(fromDate, toDate, pageNum, pageSize);
+                    JsonNode rootNode = objectMapper.readTree(jsonResponse);
+
+                    if (rootNode == null || rootNode.isEmpty()) {
+                        System.out.println("Получен пустой ответ от сервера. Прерываю работу");
+                        break;
+                    }
+
+                    if (pageNum == 1) {
+                        if (rootNode.has("pageCount")) {
+                            totalPages = rootNode.get("pageCount").asInt();
+                            int totalRecords = rootNode.has("recordCount") ? rootNode.get("recordCount").asInt() : 0;
+                            System.out.println("Всего записей в реестре c " + fromDate + " по " + toDate + ": " + totalRecords);
+                        } else {
+                            System.out.println("Ключ pageCount не найден");
+                        }
+                    }
+
+                    System.out.printf("Получение страницы %d из %d\n", pageNum, totalPages);
+
+                    String fileName = String.format("epbs_%s_%s_page_%d.json", fromDate, toDate, pageNum);
+                    ZipEntry zipEntry = new ZipEntry(fileName);
+                    zos.putNextEntry(zipEntry);
+                    String prettyJson = objectMapper.writeValueAsString(rootNode);
+                    byte[] bytes = prettyJson.getBytes(StandardCharsets.UTF_8);
+                    zos.write(bytes, 0, bytes.length);
+                    zos.closeEntry();
+
+                    pageNum++;
+                    Thread.sleep(100);
+
+                } catch (Exception e) {
+                    System.err.println("\nОшибка: Сбой на странице " + pageNum + ": " + e.getMessage());
                     break;
                 }
-
-                if (pageNum == 1) {
-                    if (rootNode.has("pageCount")) {
-                        totalPages = rootNode.get("pageCount").asInt();
-                        int totalRecords = rootNode.has("recordCount") ? rootNode.get("recordCount").asInt() : 0;
-                        System.out.println("Всего записей в реестре c " + fromDate + " по " + toDate + ": " + totalRecords);
-                    } else {
-                        System.out.println("Ключ pageCount не найден");
-                    }
-                }
-
-                System.out.printf("Получение страницы %d из %d\n", pageNum, totalPages);
-
-                String fileName = String.format(registryDirName + "/epbs_%s_%s_page_%d.json", fromDate, toDate, pageNum);
-                File outputFile = new File(fileName);
-                objectMapper.writeValue(outputFile, rootNode);
-                System.out.println("Сохранено в: " + registryDirName + "/" + outputFile.getName());
-
-                pageNum++;
-                Thread.sleep(100);
-
-            } catch (Exception e) {
-                System.err.println("\nОшибка: Сбой на странице " + pageNum + ": " + e.getMessage());
-                break;
             }
+
+        } catch (IOException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            throw e;
         }
 
-        System.out.println("Сканирование успешно завершено. Все страницы сохранены в каталоге " + registryDirName);
+        System.out.println("Сканирование успешно завершено. Все страницы сохранены в " + zipFilePath);
     }
 }
